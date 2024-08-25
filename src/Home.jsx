9638fbea9216ff4095b7a50c571483d8cbb1e160
Ani no <bryanlomerioanino@gmail.com>
2024-08-25 23:48:34 +0800

    added home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import recycleBinImage from './assets/recycle-bin.png';
+import './Home.css'; 
 
 function HomePage() {
+  const handleRecycleBinClick = () => {
+    alert('Recycle Bin Clicked!'); 
+  };
+
   return (
-    <div>
-      <h1>HIIII</h1>
+    <div className="homePage">
+      <h1>Hi, I'm Bryan Lomerio</h1>
+      <img
+        src={recycleBinImage}
+        alt="Recycle Bin"
+        className="recycle-bin-img"
+        onClick={handleRecycleBinClick}
+      />
     </div>
   );
 }
